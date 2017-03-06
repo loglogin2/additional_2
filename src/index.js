b649d9744d7747a3ee2108d30cf79202da642296
loglogin2 <i9991559@gmail.com>
2017-03-06 19:00:47 +0300

    Flatten/feature: implement all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function flatten(array) {
   // your solution
+    if (!Array.isArray(array)) {
+        return [];
+    }
+    var ret = [];
+    function flat(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (!Array.isArray(arr[i])) {
+                ret.push(arr[i]);
+            }
+            else {
+                flat(arr[i]);
+            }
+
+        }
+        return ret;
+    }
+    return flat(array);
 }
